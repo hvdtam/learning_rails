db4737bfc65daab3b5810b273fc1d71275ada9f7
Van Dinh Tam HUYNH <hvdtam@gmail.com>
2021-07-07 17:22:35 +0700

    Add error page

--- config/routes.rb
+++ config/routes.rb
@@ -25,5 +25,8 @@ Rails.application.routes.draw do
   resources :tweets
   get "users", to: "users#index"
   post "users/import", to: "users#import"
+  get "users/import", to: "users#index"
   root to: "main#index"
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
 end
